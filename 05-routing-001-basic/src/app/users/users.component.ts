dc8cbdaebb9e6d4fc3a9689607932b0349f08eb6
Paolo <Paolocargnin@boom.ink>
2020-01-31 12:50:30 +0100

    basic

---
+++ 05-routing-001-basic/src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  users = [
+    {
+      id: 1,
+      name: 'Paolo'
+    },
+    {
+      id: 2,
+      name: 'Diana'
+    },
+    {
+      id: 3,
+      name: 'Adriana'
+    }
+  ];
+}
